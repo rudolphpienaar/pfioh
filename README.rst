20e1e0ed04ad4e7867fc84505b2a1615e68430c5
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2019-07-30 15:20:31 -0400

    Fix import on mount_dir module and bump to 2.1.0.4

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ####################
-pfioh - v2.1.0.0
+pfioh - v2.1.0.4
 ####################
 
 .. image:: https://badge.fury.io/py/pfioh.svg
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfioh',
-      version          =   '2.1.0.0',
+      version          =   '2.1.0.4',
       description      =   'Path-and-File-IO-over-HTTP',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
